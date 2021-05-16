b79769656269680ea86f0d0b64f2097a489ac04a
EP <kjuioqq8@gmail.com>
2021-05-16 22:54:02 +0900

    Initial commit

---
+++ src/main/java/com/log/test/demo/scheduler/TestScheduler.java
@@ -0,0 +1,23 @@
+package com.log.test.demo.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TestScheduler {
+
+
+    @Scheduled(fixedDelay = 30000)
+    public void test(){
+
+        log.trace("hello trace scheduler");
+        log.info("hello info scheduler");
+        log.debug("hello debug scheduler");
+        log.warn("hello warn scheduler");
+        log.error("hello error scheduler");
+    }
+
+}
